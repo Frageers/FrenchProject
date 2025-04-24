9682b8b36f463b371227c869ea3eddd7ad0e7a53
Frageers <146883658+Frageers@users.noreply.github.com>
2025-04-23 20:25:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Simple script for smooth scroll (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
